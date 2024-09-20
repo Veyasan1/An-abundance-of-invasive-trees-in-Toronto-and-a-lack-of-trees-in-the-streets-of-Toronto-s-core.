4ea9b7cb2d88d32795aa2b7887c7334e0226555d
Veyasan Ragulan <veyasan.ragulan@gmail.com>
2024-09-20 15:20:51 -0400

    Updated readme, changed the author bits as well, and removed the models folder

--- scripts/05-replications.R
+++ scripts/05-replications.R
@@ -1,11 +1,11 @@
 #### Preamble ####
 # Purpose: Replicated graphs from... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Veyasan Ragulan
+# Date: 20 September 2024
+# Contact: veyasan.ragulan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
