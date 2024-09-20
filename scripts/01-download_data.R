b1efa9705111f9288dd60d99502ec00c7c234012
Veyasan Ragulan <veyasan.ragulan@gmail.com>
2024-09-20 16:28:41 -0400

    Added code to pull street tree data from OpenData Toronto. A new file called raw_data was called to store the download

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,16 +11,25 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# get package
+package <- show_package("6ac4569e-fd37-4cbc-ac63-db3624c5f6a2")
+package
 
+# get all resources for this package
+resources <- list_package_resources("6ac4569e-fd37-4cbc-ac63-db3624c5f6a2")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
+write_csv(data, "data/raw_data/raw_data.csv") 
          
