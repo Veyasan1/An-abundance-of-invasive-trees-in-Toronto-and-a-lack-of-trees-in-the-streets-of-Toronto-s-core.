b384f4070cf43a52d782768d47bd2bd717c2ca85
Veyasan Ragulan <veyasan.ragulan@gmail.com>
2024-09-23 23:42:01 -0400

    Added Discussion to 2 points in paper.qmd. Added tests of is na on ward and botanical name in the test script. Updated references with new sources, added figures to replication script

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,5 +1,5 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
+# Purpose: Tests the dataset for obvious errors
 # Author: Veyasan Ragulan
 # Date: 20 September 2024
 # Contact: veyasan.ragulan@mail.utoronto.ca
@@ -10,6 +10,15 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+data <- read_csv("data/raw_data/simulated.csv")
+
+# Test for negative numbers
+data$DBH_TRUNK |> min() <= 0
+
+# Test for NAs
+all(is.na(data$WARD))
+all(is.na(data$BOTANICAL_NAME))
+
--- scripts/05-replications.R
+++ scripts/05-replications.R
@@ -13,7 +13,40 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Load data ####
-# [...ADD CODE HERE...]
+analysis_data <- read_csv(here::here("data/analysis_data/analysis_data.csv"))
 
+### Replicate figures ###
+
+# Figure 1: Street tree count of 10 most popoulus tree species in Toronto up to September 2024
+top_species_count <- analysis_data %>%
+  count(botanical_name, sort = TRUE) %>%
+  head(10)
+
+top_species_count |> 
+  ggplot(aes(y = botanical_name, x = n)) +
+  geom_col() +
+  theme_minimal() +
+  labs(x = "Planted Trees",
+       y = NULL)
+
+# Figure 2: Street tree count by Toronto wards up to September 2024
+ward_count <- analysis_data %>%
+  count(ward, sort = TRUE) %>%
+  head(10)
+
+ward_count |> 
+  ggplot(aes(x = ward, y = n)) +
+  geom_col() +
+  theme_minimal() +
+  labs(x = "Ward",
+       y = "Number of trees planted")
+
+# Figure 3: Distribution of Distance-at-Breast-Height diameter of all trees in Toronto up to September 2024
+analysis_data |> 
+  ggplot(mapping = aes(x = dbh_trunk)) +
+  geom_histogram() +
+  theme_minimal() +
+  labs(x = "DBH Diameter",
+       y = "Tree Count")
 
 
